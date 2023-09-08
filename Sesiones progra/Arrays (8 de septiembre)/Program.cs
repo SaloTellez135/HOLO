0f4b369c4a4aafa6df3909c6c7b44e85cfb51425
SaloTellez135 <salomontellez135xd@gmail.com>
2023-09-08 12:56:46 -0500

    Arrays 8 septiembre
    Ejercicio nombnre de wiggas y dinos sin terminar, falta terminar el método

---
+++ Sesiones progra/Arrays (8 de septiembre)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays__8_de_septiembre_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ciudades = { "Osaka", "Tokyo", "Seúl", "Puebla", "Montreal", "Paris", "London", "Buenos Aires" };
+
+            int cantElems = ciudades.Length;
+            Console.WriteLine("El arreglo tiene " + cantElems + " elementos");
+            
+            string ciudad1 = ciudades[1];
+            string ciudad4 = ciudades[4];
+            Console.WriteLine(ciudad1);
+            Console.WriteLine(ciudad4);
+            Console.WriteLine(ciudades[0]);
+            Console.WriteLine(ciudades[5]);
+            ciudades[4] = "Otawa";
+            Console.WriteLine(ciudades[4]);
+
+            for(int i = 0; i < 8; i++)
+            {//harcodeado el 8 :ccc
+
+            }
+            for(int i = 0;i< ciudades.Length; i++)
+            {//Parametrizado :D
+                Console.WriteLine(i + " - " + ciudades[i]);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Sesiones progra/ArrelgoDeNombres/GeneradorNombre.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrelgoDeNombres
+{
+    class GeneradorNombre
+    {
+
+
+        public string[] prefijo = { "Chomp", "Bite", "Titan", "Mega" };
+        public string[] subfijo = { "Plodicus", "Donnicus", "Zilla", "Saur" };
+
+        public void generar(int PrimerNombre, int SegundoNombre)
+        {
+            Console.WriteLine("Tu nombre de dinosaurio es " + prefijo[PrimerNombre] + subfijo[SegundoNombre]);
+        }
+        
+
+    }
+}
