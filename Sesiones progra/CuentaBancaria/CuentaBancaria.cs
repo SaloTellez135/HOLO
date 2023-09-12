b8c7ce9939d092273ea0e37b79d0e99e698bb486
SaloTellez135 <salomontellez135xd@gmail.com>
2023-09-12 14:55:20 -0500

    Cuenta bancaria terminado (12 septiembre)
    Ambatukam creo que ya sirve el fokin codigo

---
+++ Sesiones progra/CuentaBancaria/CuentaBancaria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuentaBancaria
+{
+    class CuentaBancaria
+    {
+        private int pin;
+        private double saldo;
+
+        
+        public void setPin(int valor)
+        {
+            pin = valor;
+        }
+        public int getPin()
+        {
+            return pin;
+        }
+
+        public void setSaldo(double valor)
+        {
+            saldo = valor;
+        }
+        public double getSaldo()
+        {
+            if(pin != 0)
+            {
+                return saldo;
+                Console.WriteLine("Su saldo es " + saldo);
+                Console.Read();
+            }
+            
+            else
+            {
+                Console.WriteLine("No se ha establecido un pin, establezcalo para conocer su saldo");
+                return 0;
+            }
+        }
+
+
+    }
+}
---
+++ Sesiones progra/CuentaBancaria/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuentaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CuentaBancaria Cuenta1 = new CuentaBancaria();
+            Cuenta1.setPin(1234);
+            Cuenta1.getPin();
+            Cuenta1.setSaldo(1456.2);
+            Cuenta1.getSaldo();
+
+
+        }
+    }
+}
